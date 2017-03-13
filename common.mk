8bf8df202fa20b2817bb86385b18695bb0e527e5
filiprrs <filiprrs@gmail.com>
2017-03-13 15:12:36 -0700

    universal7880: Import usb type-c hal
    To be used with the new kernel, which ships with typec driver.
    This is a squashed commit:
    
    universal7880: USB HAL: initial type-c hal
    
    Bug: 36097487
    Test: Manually tested role swap and UI behavior
    Change-Id: I5ce520da521e0714001b60164ac850dddbd644c3
    
    universal7880: The IUsb interface uses "default" service name
    
    By default, it is expected that the service does not rename itself
    unless there is more than one service exposed by the interface. This
    CL changes the usb hal service name back to "default" instead of
    "usb_hal"
    
    Bug: 36097487
    Test: Manually tested role swap and UI behavior
    Change-Id: I4a0aa4423c80cd893c578bc58b9276956daba732
    
    universal7880: USB: HIDL: check the status of registerAsService
    
    registerAsService call could fail. Hence check the return value.
    
    Bug: 36704362
    Change-Id: I46dcd74dbee6aba72cf344f10854e5d4b1b6de18
    
    universal7880: USB: HAL: update hal to V1_1
    
    This CL adds support to notify frameworks when an audio
    or debug accessory is attached.
    
    Bug: 36604276
    Test: Manually test inserting an Audio accessory.
    Change-Id: I9ec0f88ed85ed593b31132ba58bfedb51a435cd8
    
    universal7880: Usb: HAL: Update wahoo HAL
    
    This CL rebases wahoo HAL based on the kernel change to update
    sysfs interface.
    
    https://partner-android-review.googlesource.com/#/c/836088/
    
    Bug: 62272992
    Test: Verify USB dialog on wahoo
    Change-Id: I8bc8f494176c7648abeb9783fbd18dc837793bda
    
    universal7880: USB HAL: Use 1.1 IUsb intead of 1.0 IUsb
    
    Use 1.1 IUsb in order to make register the service as 1.1 compatible.
    This enables lshal to recognize the hal service as 1.1 implementation.
    
    Bug: 62917546
    Test: Manually verified lshal output
    android.hardware.usb@1.0::IUsb/default   0/1             782     6024 568
    android.hardware.usb@1.1::IUsb/default   0/1             782     6024 568
    
    Change-Id: I9b0817841dbcc345a1d854ee4a5193797181ea3f
    
    universal7880: USB: HAL: run as user root, group root system
    
    This allows USB HAL to write USB sysfs nodes.
    
    Bug: 38352281
    Change-Id: Iaf8f1bad1a0394e553996b7a42bc34745b769b16
    
    universal7880: USB: HAL: enable auto suspend for USB headsets
    
    Adds a thread that handles add uevents of USB devices, and
    enables auto suspend on that USB device (i.e. set power/control
    to auto) if the device idProduct/idVendor is whitelisted.
    
    The android kernel will already autosuspend audio devices,
    however this enables autosuspend for the Google USB-C to
    3.5mm adapter, which presents an HID-only interface when
    no 3.5mm headset is connected.
    
    Test: with the selinux and .rc changes for access permission
    - MIR without headset: power/control set to auto
    - MIR with headset: power/control set to auto
    - regular mouse: power/control set to on
    
    Bug: 38352281
    Change-Id: I81572584ea02f6bdc814e70ab3439ab86c34a50a
    
    universal7880: USB: Fix potential racing by wall time change in USBhal
    
    pthread_cond_timedwait used wall time so it might introduce racing,
    pthread_cond_timedwait_monotonic_np is Android specific but it is being
    deprecated. And now Android support pthread_condattr_setclock so use it
    to wait on CLOCK_MONOTONIC instead.
    
    Bug: 64623895
    Test: USB switch function works, charging/MTP/PTP
    Change-Id: I136533ff90ef1be2b042ef1e0829643f2f7aa968
    
    universal7880: USB: Add 'vendor.' prefix to a vendor HAL service name
    
    To prevent property name collisions between properties of system and
    vendor, 'vendor.' prefix must be added to a vendor HAL service name.
    You can see the details in http://go/treble-sysprop-compatibility.
    
    Test: succeeded building and tested on a walleye device
    Bug: 36796459
    Change-Id: Idc995c550657feca5e0f934444a31805bc3538f9
    
    universal7880: USB: Use -Werror
    
    * Remove unused variables.
    * Return result of registerAsService().
    
    Bug: 66996870
    Test: build with WITH_TIDY=1
    Change-Id: I79d1add6190e835de20a0b0c247bbbb259096c14
    
    universal7880: Rename USB HAL suffix to universal7880
    
    Change-Id: I6b028f5efceee8e21f7684f99aa8b7e1c49b1b29

--- common.mk
+++ common.mk
@@ -295,7 +295,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.1-service.universal7880
 
 # HIDL Manifest
 PRODUCT_COPY_FILES += \
