1f7dcedf61eeaad1be9c7692c23146781d45d0f2
Pawin Vongmasa <pawin@google.com>
2017-05-03 19:54:12 -0700

    universal7880: Create media_profiles_V1_0.xml
    media_profiles_V1_0.xml is created from media_profiles.xml by replacing
    the dtd schema inside media_profiles.xml with an external reference to
    /system/etc/media_profiles_V1_0.dtd.
    
    Change-Id: I0fbb03603a72d0108a96bfa0f0a4ceb12ef3d430

--- common.mk
+++ common.mk
@@ -216,7 +216,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml  \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles.xml
+    $(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
 # GPS
 PRODUCT_PACKAGES += \
