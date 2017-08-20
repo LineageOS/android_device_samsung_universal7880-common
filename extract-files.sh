964635931ad4f9e6bd57707c0b6ead2dfd37c0dd
Christopher N. Hesse <raymanfx@gmail.com>
2017-08-20 21:40:16 +0200

    extract-files: Try destination for non-adb usecases as well

--- extract-files.sh
+++ extract-files.sh
@@ -21,13 +21,17 @@ for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
 
     if [ -z "$STOCK_ROM_DIR" ]; then
         adb pull /system/$FILE $BASE/$DEST
-        # if file does not exist try destination
-        if [ "$?" != "0" ]
-        then
-            adb pull /system/$DEST $BASE/$DEST
-        fi
     else
         cp $STOCK_ROM_DIR/$FILE $BASE/$DEST
+    fi
+
+    # if file does not exist try destination
+    if [ "$?" != "0" ]
+    then
+        if [ -z "$STOCK_ROM_DIR" ]; then
+            adb pull /system/$DEST $BASE/$DEST
+        else
+            cp $STOCK_ROM_DIR/$DEST $BASE/$DEST
     fi
 done
 
