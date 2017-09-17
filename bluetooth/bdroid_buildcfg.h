3f8d1523876884197fa61b8a99d713d735f11e7e
Christopher N. Hesse <raymanfx@gmail.com>
2017-09-17 14:13:54 +0200

    a5y17lte: Enable BT WBS
    This will allow for 16kHz wideband BT SCO voice calls.

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -22,4 +22,7 @@
 #define BLUETOOTH_QTI_SW TRUE
 #define BLE_VND_INCLUDED   TRUE
 
+#define BTM_WBS_INCLUDED TRUE       /* Enable WBS */
+#define BTIF_HF_WBS_PREFERRED TRUE  /* Use WBS    */
+
 #endif
