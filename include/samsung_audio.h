b243b1f0a1b20db0cea5d64850c0668f306f9d83
Christopher N. Hesse <raymanfx@gmail.com>
2017-10-16 20:12:08 +0200

    a5y17lte: Update audio header
     * Enable TFA amplifier support.

--- include/samsung_audio.h
+++ include/samsung_audio.h
@@ -49,5 +49,45 @@
 #define SOUND_CAPTURE_HOTWORD_DEVICE 0
 */
 
+/*
+ * If the device has stereo speakers and the speakers are arranged on
+ * different sides of the device you can activate this feature by
+ * setting it to 1.
+ */
+#define SWAP_SPEAKER_ON_SCREEN_ROTATION 0
+
+/*
+ * You can that this to 1 if your kernel supports irq affinity for
+ * fast mode. See /proc/asound/irq_affinity
+ */
+#define SUPPORTS_IRQ_AFFINITY 0
+
+/*
+ * The Wolfson/Cirruslogic chips need to shutdown the DAPM route completely
+ * to be able to load a new firmware. Some of these chips need a delay after
+ * shutodown to full poweroff the DSPs.
+ *
+ * A good value to start with is 10ms:
+ *
+ * #define DSP_POWEROFF_DELAY 10 * 1000
+ */
+/* #define DSP_POWEROFF_DELAY 0 */
+
+/*
+ * Some device variants (often T-Mobile) have a separate voice processing IC
+ * (Audience EarSmart xxx).
+ * This hooks into the voice call session and enables, configures and disables
+ * this extra firmware so RX/TX streams can be routed by the driver.
+ */
+/* #define AUDIENCE_EARSMART_IC */
+
+/*
+ * Some device variants have an external NXP TFA amplifier chip.
+ * The code might be shareable with other possible amplifier implementations
+ * (e.g. Maxim), but for now it is only tested with a TFA9896.
+ * This hooks into the speaker usecases and handles the i2c device state by
+ * hooking into the stock library which exposes the whole interface.
+ */
+#define TFA_AMPLIFIER 1
 
 #endif // SAMSUNG_AUDIO_H
