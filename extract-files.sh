1a075ae4bad2eec41128b8958364e96cb09deca0
Christopher N. Hesse <raymanfx@gmail.com>
2018-01-03 14:00:45 +0100

    a5y17lte: Switch to common extract-utils

--- extract-files.sh
+++ extract-files.sh
@@ -1,39 +1,56 @@
 #!/bin/bash
+#
+# Copyright (C) 2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
 
 VENDOR=samsung
 DEVICE=a5y17lte
 
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $BASE/*
+# Load extractutils and do some sanity checks
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
-    OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
-    FILE=${PARSING_ARRAY[0]}
-    DEST=${PARSING_ARRAY[1]}
-    if [ -z $DEST ]
-    then
-        DEST=$FILE
-    fi
-    DIR=`dirname $FILE`
-    if [ ! -d $BASE/$DIR ]; then
-        mkdir -p $BASE/$DIR
-    fi
+CM_ROOT="$MY_DIR"/../../..
 
-    if [ -z "$STOCK_ROM_DIR" ]; then
-        adb pull /system/$FILE $BASE/$DEST
-    else
-        cp $STOCK_ROM_DIR/$FILE $BASE/$DEST
-    fi
+HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
 
-    # if file does not exist try destination
-    if [ "$?" != "0" ]
-    then
-        if [ -z "$STOCK_ROM_DIR" ]; then
-            adb pull /system/$DEST $BASE/$DEST
-        else
-            cp $STOCK_ROM_DIR/$DEST $BASE/$DEST
-        fi
-    fi
-done
+if [ $# -eq 0 ]; then
+  SRC=adb
+else
+  if [ $# -eq 1 ]; then
+    SRC=$1
+  else
+    echo "$0: bad number of arguments"
+    echo ""
+    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
+    echo ""
+    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
+    echo "the device using adb pull."
+    exit 1
+  fi
+fi
 
-./setup-makefiles.sh
+# Initialize the helper
+setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
+
+extract "$MY_DIR"/proprietary-files.txt "$SRC"
+
+"$MY_DIR"/setup-makefiles.sh
