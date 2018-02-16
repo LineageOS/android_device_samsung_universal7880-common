9c917c4256fd8a9b0ade3db1ee28ad641e0cf860
Christopher N. Hesse <raymanfx@gmail.com>
2018-02-16 01:10:10 +0100

    universal7880: Move cbd to proprietary blobs

--- common.mk
+++ common.mk
@@ -34,10 +34,6 @@ PRODUCT_PACKAGES += \
     init.wifi.rc \
     ueventd.samsungexynos7880.rc
 
-# cpboot-daemon
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/ramdisk/cbd:root/sbin/cbd
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
