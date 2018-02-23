f386b0f2442eb6a546ace227e2636ac23c7767e0
Christopher N. Hesse <raymanfx@gmail.com>
2018-02-23 21:36:00 +0100

    universal7880: Transition to TARGET_LD_SHIM_LIBS
    Change-Id: I5b4c06314c885c415f69819d4890010795b1a4f0

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -224,3 +224,6 @@ BOARD_SECCOMP_POLICY += device/samsung/universal7880-common/seccomp
 
 # SELinux
 BOARD_SEPOLICY_DIRS += device/samsung/universal7880-common/sepolicy
+
+# Shims
+TARGET_LD_SHIM_LIBS := /system/lib64/libbauthserver.so|libbauthtzcommon_shim.so:/system/lib/libcamera_client.so|libcamera_client_shim.so:/system/lib/libexynoscamera.so|libexynoscamera_shim.so:/system/lib64/libstagefright.so|libstagefright_shim.so:/system/lib/libstagefright.so|libstagefright_shim.so
