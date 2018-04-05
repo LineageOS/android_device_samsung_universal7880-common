411bf1cbab26df09d1072ff294a8aae2f611ab0e
Simon Shields <simon@lineageos.org>
2018-04-05 15:55:39 +1000

    universal7880: enable separated DT
    each device should specify their own prebuilt DT image for now.
    
    Change-Id: Id4a60eb046312e14272e893b823486267b809351

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -51,7 +51,7 @@ BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_IMAGE_NAME := Image
 #BOARD_KERNEL_CMDLINE := The bootloader ignores the cmdline from the boot.img
-#BOARD_KERNEL_SEPARATED_DT := true
+BOARD_KERNEL_SEPARATED_DT := true
 #TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
 
 # Kernel
