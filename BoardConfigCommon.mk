2b63ae3a328d79e1d999c2f1eee90862b3b9c845
filiprrs <filiprrs@gmail.com>
2018-06-15 14:54:17 +0200

    universal7880: Use dtbhtoolExynos
    Change-Id: Ice26e9794f2b8bb80b075fb8771bf999e6bf5efd

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -46,13 +46,14 @@ TARGET_2ND_CPU_VARIANT := cortex-a53
 TARGET_USES_64_BIT_BINDER := true
 
 # Extracted with libbootimg
+BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_IMAGE_NAME := Image
 #BOARD_KERNEL_CMDLINE := The bootloader ignores the cmdline from the boot.img
 BOARD_KERNEL_SEPARATED_DT := true
-#TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
+TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
 
 # Kernel
 TARGET_KERNEL_ARCH := arm64
