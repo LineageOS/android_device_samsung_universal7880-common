2d0314ab5827c708da973b38cc926d5007c41599
Michael Bestas <mkbestas@lineageos.org>
2018-07-26 15:43:15 +0200

    universal7880: Patchelf blobs to remove deprecated dependencies
    * Remove android.hidl.base@1.0, it is included in libhidltransport since O-MR1
    
    Change-Id: I9338cfeb236b4b37e14c5d4e42af978ea8d54016

--- extract-files.sh
+++ extract-files.sh
@@ -54,4 +54,11 @@ setup_vendor "$DEVICE_COMMON" "$VENDOR" "$LINEAGE_ROOT" true
 extract "$MY_DIR"/proprietary-files.txt "$SRC"
 extract "$MY_DIR"/proprietary-files-bsp.txt "$SRC"
 
+# Remove dependencies that are not used in lineage 
+BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE_COMMON"/proprietary
+
+for HIDL_BASE_LIB in $(grep -lr "android\.hidl\.base@1\.0\.so" $BLOB_ROOT); do
+    patchelf --remove-needed android.hidl.base@1.0.so "$HIDL_BASE_LIB" || true
+done
+
 "$MY_DIR"/setup-makefiles.sh
