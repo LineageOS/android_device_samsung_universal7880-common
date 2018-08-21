1e0853a4f69fa05c90a23fc3e02d798cb95c7498
filiprrs <filiprrs@gmail.com>
2018-08-21 23:52:39 +0200

    universal7880: O blobs: GPS
    Change-Id: I4c142b76387cd0b0cf6400ee4c916b4add5020b6

--- common.mk
+++ common.mk
@@ -215,8 +215,8 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/gps/gps.cfg:system/etc/gps.cfg \
-    $(LOCAL_PATH)/configs/gps/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/configs/gps/gps.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/gnss/gps.cfg \
+    $(LOCAL_PATH)/configs/gps/gps.conf:system/etc/gps.conf
 
 # Keys
 PRODUCT_COPY_FILES += \
