38e9a23caa22659cc1808e3ab77f997820d8621d
Ethan Chen <intervigil@gmail.com>
2018-11-30 20:24:36 +0100

    universal7880: Add camera shim for Fence destructor
    * This was replaced with a default destructor which has no visible
      linkage. No-op the destructor call and hope the actual destructor is
      called from underneath.
    
    Change-Id: Id039e916c24959e9f60391bc10886df878f4d265

--- shims/libexynoscamera/Android.mk
+++ shims/libexynoscamera/Android.mk
@@ -16,7 +16,9 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := GraphicBuffer.cpp
+LOCAL_SRC_FILES := \
+    GraphicBuffer.cpp \
+    Fence.cpp
 
 LOCAL_C_INCLUDES := frameworks/native/include
 
