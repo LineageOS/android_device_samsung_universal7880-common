cfd42299cb8fc20b9c75d26571583f33e00f964a
filiprrs <filiprrs@gmail.com>
2019-07-15 16:06:48 +0200

    universal7880: Switch to pie blobbed graphics
    Change-Id: If0b5aeba44d66c9970624889335b698d11ebdb64

--- common.mk
+++ common.mk
@@ -84,11 +84,9 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.renderscript@1.0-impl \
     gralloc.exynos5 \
-    libhwc2on1adapter \
     memtrack.exynos5
 
 PRODUCT_PACKAGES += \
-    libion \
     libfimg
 
 # Camera
