e67a7748e2fccdd32b64b63bda431a4f83ec44ce
filiprrs <filiprrs@gmail.com>
2019-10-22 17:40:27 +0200

    universal7880: ramdisk: Drop obsolete eng tags
    Change-Id: I9f25e819e9f0b1dc7497b08dabaf7d8e7ed62393

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.samsungexynos7880
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := BOOT
 LOCAL_SRC_FILES    := fstab.samsungexynos7880
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -10,7 +10,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.baseband.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := BOOT
 LOCAL_SRC_FILES    := init.baseband.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -18,7 +18,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.power.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := BOOT
 LOCAL_SRC_FILES    := init.power.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -26,7 +26,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.rilchip.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := BOOT
 LOCAL_SRC_FILES    := init.rilchip.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -34,7 +34,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.samsung.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := BOOT
 LOCAL_SRC_FILES    := init.samsung.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -42,7 +42,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.samsungexynos7880.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := BOOT
 LOCAL_SRC_FILES    := init.samsungexynos7880.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -50,7 +50,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.samsungexynos7880.usb.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := BOOT
 LOCAL_SRC_FILES    := init.samsungexynos7880.usb.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -58,7 +58,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.wifi.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := BOOT
 LOCAL_SRC_FILES    := init.wifi.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -66,7 +66,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.wifi_qcom.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := BOOT
 LOCAL_SRC_FILES    := init.wifi_qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
@@ -74,7 +74,7 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.samsungexynos7880.rc
-LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := BOOT
 LOCAL_SRC_FILES    := ueventd.samsungexynos7880.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
