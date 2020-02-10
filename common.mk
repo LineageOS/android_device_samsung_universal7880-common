dc34833d82957775defe199bd06b3f90f443e9db
ivanmeler <i_ivan@windowslive.com>
2020-02-10 17:03:22 +0100

    universal7880: Switch to hidl power hal
    Change-Id: Ief0b12222e8f84706aa6c5326c41aa9d5e208643

--- common.mk
+++ common.mk
@@ -244,8 +244,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    power.universal7880
+    android.hardware.power@1.0-service.exynos
 
 # Lights
 PRODUCT_PACKAGES += \
