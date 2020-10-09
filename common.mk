fe3977f15002ceeb57e361ada91b475d6417f878
LuK1337 <priv.luk@gmail.com>
2020-10-09 19:25:04 +0200

    universal7880: Build libhidltransport/libhwbinder
    Change-Id: I4e3b084b21a3aca8aeedf68cb3d16b51be919279

--- common.mk
+++ common.mk
@@ -315,7 +315,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.manager@1.0 \
+    libhidltransport \
+    libhwbinder
 
 # System.prop
 TARGET_SYSTEM_PROP += device/samsung/universal7880-common/system.prop
