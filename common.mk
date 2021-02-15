a25ee8b39ad8fa660a004e1e21b8d96f5840069e
filiprrs <filiprrs@gmail.com>
2021-02-15 19:00:51 +0100

    universal7880: Switch to basic usb hal
    Will work with our rebased A720S kernel.
    
    Change-Id: I5439b48482b62716c0d52384f5ec49b3667ef58f

--- common.mk
+++ common.mk
@@ -295,7 +295,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.0-service.basic
 
 # HIDL Manifest
 PRODUCT_COPY_FILES += \
