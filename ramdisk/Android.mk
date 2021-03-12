ed56fdf25c7b650ab117c477e6443af595956298
Jan Altensen <info@stricted.net>
2021-03-12 18:01:42 +0100

    universal7880: split mobicore into its own init file
     * based on whats found on s9
    
    Change-Id: Ifc9936fc472540f60935c611ab3021ca947cfcc1

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -65,6 +65,14 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := mobicore.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := mobicore.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := wifi.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
