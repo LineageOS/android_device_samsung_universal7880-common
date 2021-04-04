8f1cd4944fafc41f18b27c811ef76b955dd1733c
Mohamed Moussa <mmouss@codeaurora.org>
2021-04-04 20:28:01 +0200

    universal7880: ramdisk: Move init.recovery.samsungexynos7880.rc out of root
    The file init.recovery.samsungexynos7880.rc flagged as offending file in root/,
    because it violates the pathing req. (system/ & root/). Place the file
    in $(TARGET_RECOVERY_ROOT_OUT)
    
    Change-Id: Iefae195fec947127ffc33c013ebabca072ba81e3

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -45,7 +45,7 @@ LOCAL_MODULE       := init.recovery.samsungexynos7880.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := BOOT
 LOCAL_SRC_FILES    := init.recovery.samsungexynos7880.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
