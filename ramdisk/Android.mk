6c42947f54d96a4d727add5cfb1989aae39a7b55
Abhishek Arpure <aarpure@codeaurora.org>
2021-04-04 20:34:51 +0200

    universal7880: Move ueventd.samsungexynos7880.rc to /vendor
    Change-Id: Iab2699bf26ca4402b835d1af8d4493f9b28595d9

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -90,8 +90,9 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.samsungexynos7880.rc
+LOCAL_MODULE_STEM  := ueventd.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := BOOT
 LOCAL_SRC_FILES    := ueventd.samsungexynos7880.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
