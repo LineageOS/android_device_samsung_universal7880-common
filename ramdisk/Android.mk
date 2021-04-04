6f8244aa94aff359374d93fbf4f3ac3912dbe60f
Abhishek Arpure <aarpure@codeaurora.org>
2021-04-04 20:23:51 +0200

    universal7880: Move ueventd.samsungexynos7880.rc to /vendor
    Change-Id: Iab2699bf26ca4402b835d1af8d4493f9b28595d9

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -13,7 +13,7 @@ LOCAL_MODULE       := init.baseband.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := BOOT
 LOCAL_SRC_FILES    := init.baseband.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -29,7 +29,7 @@ LOCAL_MODULE       := init.samsung.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := BOOT
 LOCAL_SRC_FILES    := init.samsung.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -37,7 +37,7 @@ LOCAL_MODULE       := init.samsungexynos7880.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := BOOT
 LOCAL_SRC_FILES    := init.samsungexynos7880.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -53,7 +53,7 @@ LOCAL_MODULE       := init.samsungexynos7880.usb.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := BOOT
 LOCAL_SRC_FILES    := init.samsungexynos7880.usb.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -90,8 +90,9 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.samsungexynos7880.rc
+LOCAL_MODULE_STEM  := ueventd.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := BOOT
 LOCAL_SRC_FILES    := ueventd.samsungexynos7880.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
