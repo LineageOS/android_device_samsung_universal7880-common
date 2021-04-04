ee10a7e96789bf3e529b078a1ce43e0cc275374c
Abhishek Arpure <aarpure@codeaurora.org>
2021-04-04 20:23:51 +0200

    universal7880: Move hardware specific .rc files to /vendor/etc/init/hw
    The reason these files are moved to /vendor/etc/init/hw and
    not /vendor/etc/init is because 'init' scans the /vendor/etc/init folder
    for vendor service .rc fragments to import. That would either result
    in duplicate imports or out-of-order imports of these hardware
    specific .rc files and that *may* have unintended consequences.
    
    In order to preserve the current import order for MTP, we move all
    hardware specific .rc files to /vendor/etc/init/hw which is not
    autoscanned by init BUT leave the init.${ro.hardware}.rc (i.e.
    init.qcom.rc in this case) in root for 'init' to find.
    
    Change-Id: I14531da05e2a3a256f734f8de96e1f1969c753f9

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -11,9 +11,9 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.baseband.rc
 LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := BOOT
+LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.baseband.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -27,17 +27,17 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.samsung.rc
 LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := BOOT
+LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.samsung.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.samsungexynos7880.rc
 LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := BOOT
+LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.samsungexynos7880.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -51,9 +51,9 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.samsungexynos7880.usb.rc
 LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := BOOT
+LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.samsungexynos7880.usb.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
