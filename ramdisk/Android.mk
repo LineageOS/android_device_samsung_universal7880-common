697517b13d5b6af7080e7b6561f7056d9ad34250
Bruno Martins <bgcngm@gmail.com>
2021-04-04 20:21:22 +0200

    universal7880: Move fstab to /vendor
    Change-Id: Ie1c6cb662f2f54c7c14d3e7d55aedbfbeef5f8a7

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -3,9 +3,9 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.samsungexynos7880
 LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := BOOT
+LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := fstab.samsungexynos7880
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
