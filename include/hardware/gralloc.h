07cdb389e45279e0ba0d92a96f3a0edcf4822b8f
Jan Altensen <info@stricted.net>
2021-07-21 18:18:20 +0200

    universal7880: update gralloc header
    Change-Id: I7c370f91636c1813a46d5e8bf788aef9f14c51e5

--- include/hardware/gralloc.h
+++ include/hardware/gralloc.h
@@ -135,6 +135,9 @@ enum {
      * out by GRALLOC_USAGE_ALLOC_MASK, so gralloc modules will not need to
      * handle this flag. */
     GRALLOC_USAGE_FOREIGN_BUFFERS       = 0x00200000U,
+    
+    /* buffer will be used as input to HW HEIC image encoder */
+    GRALLOC_USAGE_HW_IMAGE_ENCODER      = 0x08000000U,
 
     /* Mask of all flags which could be passed to a gralloc module for buffer
      * allocation. Any flags not in this mask do not need to be handled by
